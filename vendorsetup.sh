fdf641344b4dd811aa3d4502fb47c766a10b3fc9
briant24 <briantadr24@gmail.com>
2025-07-12 08:47:25 +0000

    emerald : setup vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+cd ../../../
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-21 device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
+
+git clone https://github.com/briant24/android_vendor_xiaomi_emerald.git vendor/xiaomi/emerald
+git clone https://github.com/muhammadrafiasyddiq/android_kernel_xiaomi_emerald.git kernel/xiaomi/emerald
